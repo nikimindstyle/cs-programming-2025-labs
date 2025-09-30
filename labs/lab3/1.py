2496567a95665fd9d1d281f817d5bd2121b5a663
nikimindstyle <nikita.ananko.15062007@gmail.com>
2025-09-30 13:08:24 +1000

    classwork

---
+++ labs/lab3/1.py
@@ -0,0 +1,8 @@
+from distutils.command.build_scripts import first_line_re
+
+
+name = (input('Введите имя:'))
+age = (input('введите возраст:'))
+
+for i in range(10):
+    print(f'Меня зовут {name}, мне {age} лет')
